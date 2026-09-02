b8cb84cd354964e70fd11bc740eb90940ff57cc0
Moisés Dourado <moisesdourado.dev@gmail.com>
2026-09-02 19:21:41 -0300

    estrutura inicial do projeto Maven

---
+++ prg03moisesdourado/src/main/java/br/com/ifba/prg03moisesdourado/Prg03moisesdourado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba.prg03moisesdourado;
+
+/**
+ *
+ * @author moise
+ */
+public class Prg03moisesdourado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
