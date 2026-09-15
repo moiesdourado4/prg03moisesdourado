68ff0b168109959884f0462302cfe636a400e646
Moisés Dourado <moisesdourado.dev@gmail.com>
2026-09-15 19:40:00 -0300

    feat: adicionando os getters e setters à classe Usuario.

--- prg03moisesdourado/src/main/java/br/com/ifba/usuario/entity/Usuario.java
+++ prg03moisesdourado/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -18,4 +18,71 @@ public class Usuario {
     private String email;
     private String login;
     private String senha;
+    
+    //métodos
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
 }
