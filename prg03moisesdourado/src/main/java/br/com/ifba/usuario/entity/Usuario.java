88a9028f1033b1cc28aacf3e3cec91a53d725e96
Moisés Dourado <moisesdourado.dev@gmail.com>
2026-09-14 21:47:15 -0300

    modificando a visibilidade dos atributos para privado.

--- prg03moisesdourado/src/main/java/br/com/ifba/usuario/entity/Usuario.java
+++ prg03moisesdourado/src/main/java/br/com/ifba/usuario/entity/Usuario.java
@@ -10,12 +10,12 @@ package br.com.ifba.usuario.entity;
  */
 public class Usuario {
     //Atributos
-    public String nome;
-    public String cpf;
-    public String genero;
-    public String dataNascimento;
-    public String telefone;
-    public String email;
-    public String login;
-    public String senha;
+    private String nome;
+    private String cpf;
+    private String genero;
+    private String dataNascimento;
+    private String telefone;
+    private String email;
+    private String login;
+    private String senha;
 }
